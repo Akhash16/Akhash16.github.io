3989ff23b3e87208651f29255c42778812977f6e
Akhash S <akhash.xs@gmail.com>
2025-01-20 22:10:48 -0700

    Made the scroll reveal animation faster

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,8 +111,8 @@ window.addEventListener("scroll", scrollActive);
 const sr = ScrollReveal({
   origin: "top",
   distance: "60px",
-  duration: 2500,
-  delay: 400,
+  duration: 1000,
+  delay: 100,
   reset: true,
 });
 
